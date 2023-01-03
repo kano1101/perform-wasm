45bc113ec0559f6ca77660a3efe2853d2d111939
Akira Kano <a.kano1101@gmail.com>
2023-01-03 15:31:28 +0900

    動作

--- egui_test/src/main.rs
+++ egui_test/src/main.rs
@@ -35,89 +35,131 @@ mod ip {
     perform_wasm::build_perform!(String);
 }
 
-struct Application {
+struct SinglelineMyText {
     taker: ip::Taker,
     response: Option<String>,
     displaying_bg_color_step: f32,
+    color_weight_max: f32,
+    update_request_interval: std::time::Duration,
+    update_request_duration: std::time::Duration,
+    micro_copy: String,
+    waiting_bg_color: eframe::egui::Color32,
+    updated_bg_color: eframe::egui::Color32,
+    tone_down_speed: f32,
 }
-impl Application {
-    pub fn new(_cc: &eframe::CreationContext<'_>) -> Application {
+impl SinglelineMyText {
+    pub fn new() -> Self {
         use perform_wasm::Performer as _;
         let session = ip::Session::activate_with_spawn_local();
         let taker = ip::Taker::new(session);
-        Application {
+        Self {
             taker: taker,
             response: None,
             displaying_bg_color_step: 0.,
+            color_weight_max: 255.,
+            update_request_interval: std::time::Duration::from_millis(250),
+            update_request_duration: std::time::Duration::from_millis(16),
+            micro_copy: "Now loading...".to_string(),
+            waiting_bg_color: eframe::egui::Color32::DARK_GREEN,
+            updated_bg_color: eframe::egui::Color32::LIGHT_BLUE,
+            tone_down_speed: 8.,
+        }
+    }
+    fn update(&mut self, ctx: &eframe::egui::Context, ui: &mut eframe::egui::Ui) {
+        let fut = async {
+            reqwest::get("http://httpbin.org/ip")
+                .await
+                .unwrap()
+                .text()
+                .await
+                .unwrap()
+        };
+        if self.is_take_required() {
+            let took = self.taker.try_take(fut);
+            self.take_necessity(took);
+        }
+        self.response
+            .as_ref()
+            .or_else(|| {
+                self.displaying_bg_color_step = self.color_weight_max;
+                ctx.request_repaint_after(self.update_request_interval);
+                log::trace!("Request repaint again!");
+                Some(&self.micro_copy)
+            })
+            .and_then(|state| {
+                let highlight_color = self.current_bg_color().to_array();
+
+                let mut tone = self.displaying_bg_color_step - self.tone_down_speed;
+                if tone < 0. {
+                    self.displaying_bg_color_step = 0.;
+                    tone = 0.;
+                } else {
+                    self.displaying_bg_color_step = tone;
+                    ctx.request_repaint_after(self.update_request_duration);
+                }
+                log::trace!("tone: {}", tone);
+
+                let visuals = ui.visuals_mut();
+
+                let base_color = visuals.extreme_bg_color.to_array();
+                let mut finish_color = visuals.extreme_bg_color.to_array();
+
+                use itertools::izip;
+                for (base, highlight, finish) in
+                    izip!(&base_color, &highlight_color, &mut finish_color)
+                {
+                    let current_weight = tone;
+                    let diff = *highlight as f32 - *base as f32;
+                    let rate = current_weight / self.color_weight_max;
+                    *finish = (*base as f32 + (diff * rate)) as u8;
+                }
+
+                let [r, g, b, _a] = finish_color;
+                let finish_color = eframe::egui::Color32::from_rgb(r, g, b);
+
+                visuals.extreme_bg_color = finish_color;
+
+                let mut state = state.clone();
+                ui.text_edit_singleline(&mut state);
+                let is_enter_pressed = ui
+                    .input_mut()
+                    .consume_key(eframe::egui::Modifiers::NONE, eframe::egui::Key::Enter);
+                if is_enter_pressed {
+                    self.displaying_bg_color_step = self.color_weight_max;
+                }
+                Some(state)
+            });
+    }
+    fn current_bg_color(&mut self) -> eframe::egui::Color32 {
+        if self.color_weight_max == self.displaying_bg_color_step {
+            self.waiting_bg_color
+        } else {
+            self.updated_bg_color
+        }
+    }
+    fn is_take_required(&self) -> bool {
+        self.response.is_none()
+    }
+    fn take_necessity(&mut self, took: Option<String>) {
+        self.response = took
+    }
+}
+
+struct Application {
+    text: SinglelineMyText,
+}
+impl Application {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self {
+            text: SinglelineMyText::new(),
         }
     }
 }
+impl Application {}
 impl eframe::App for Application {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
         eframe::egui::CentralPanel::default().show(ctx, |ui: &mut eframe::egui::Ui| {
-            let fut = async {
-                reqwest::get("http://httpbin.org/ip")
-                    .await
-                    .unwrap()
-                    .text()
-                    .await
-                    .unwrap()
-            };
-            if let None = self.response {
-                self.response = self.taker.try_take(fut);
-            }
-            let micro = "Now loading...".to_string();
-            let color_weight_max = 255.;
-            self.response
-                .as_ref()
-                .or_else(|| {
-                    self.displaying_bg_color_step = color_weight_max;
-                    let dur = std::time::Duration::from_millis(250);
-                    ctx.request_repaint_after(dur);
-                    log::debug!("Request repaint again!");
-                    Some(&micro)
-                })
-                .and_then(|state| {
-                    let highlight_color = if color_weight_max == self.displaying_bg_color_step {
-                        eframe::egui::Color32::LIGHT_RED.to_array()
-                    } else {
-                        eframe::egui::Color32::DARK_GREEN.to_array()
-                    };
-
-                    let tone_down_speed = 8.;
-                    let mut tone = self.displaying_bg_color_step - tone_down_speed;
-                    if tone < 0. {
-                        self.displaying_bg_color_step = 0.;
-                        tone = 0.;
-                    } else {
-                        self.displaying_bg_color_step = tone;
-                        let dur = std::time::Duration::from_millis(16);
-                        ctx.request_repaint_after(dur);
-                    }
-                    log::debug!("tone: {}", tone);
-
-                    let visuals = ui.visuals_mut();
-                    let base_color = visuals.extreme_bg_color.to_array();
-                    let mut finish_color = visuals.extreme_bg_color.to_array();
-
-                    use itertools::izip;
-                    for (base, highlight, finish) in
-                        izip!(&base_color, &highlight_color, &mut finish_color)
-                    {
-                        let current_weight = tone;
-                        let diff = *highlight as f32 - *base as f32;
-                        let rate = current_weight / color_weight_max;
-                        *finish = (*base as f32 + (diff * rate)) as u8;
-                    }
-                    let [r, g, b, _a] = finish_color;
-                    let finish_color = eframe::egui::Color32::from_rgb(r, g, b);
-                    visuals.extreme_bg_color = finish_color;
-
-                    let mut state = state.clone();
-                    ui.text_edit_singleline(&mut state);
-                    log::debug!("Color change testing");
-                    Some(state)
-                });
+            self.text.update(ctx, ui);
         });
     }
 }
